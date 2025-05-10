4e61c72529b9bafcf6746319b25015cf78513e4b
João Vítor Sgotti Veiga <jvsgottiveiga@gmail.com>
2025-05-09 22:25:39 -0300

    FIX: AGOORA EU ACHO QUE LIBEROU A DEPENDENCIA

--- database_api.py
+++ database_api.py
@@ -7,7 +7,14 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
+
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+
+SQLALCHEMY_DATABASE_URL = f'postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
 
 # echo=True é útil para debug, loga as queries SQL. Remova em produção.
 engine = create_engine(SQLALCHEMY_DATABASE_URL, echo=True)
