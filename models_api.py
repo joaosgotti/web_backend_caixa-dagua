28af00ff716e474429a7650e5393f507789e6fd6
joaosgotti <jvsgottiveiga@gmail.com>
2025-10-09 13:46:33 -0300

    .env

--- models_api.py
+++ models_api.py
@@ -4,13 +4,12 @@ from sqlalchemy import Column, Integer, Float, DateTime
 from database_api import Base 
 from pydantic import BaseModel
 
-class Leitura(Base): # Modelo SQLAlchemy
+class Leitura(Base):
     __tablename__ = "leituras" 
 
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
     distancia = Column(Float, nullable=False)
     created_on = Column(DateTime(timezone=True), nullable=False)
-    #nivel = Column(Float, nullable=False) # No banco, é Float e não nulo
 
     def __repr__(self):
         return f"<Leitura(id={self.id}, distancia={self.distancia}, created_on='{self.created_on}', nivel={self.nivel})>"
@@ -19,4 +18,3 @@ class LeituraResponse(BaseModel):
     id: int
     distancia: float 
     created_on: str  
-    #nivel: Optional[Union[int, float]]
